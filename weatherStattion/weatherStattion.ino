0cb8248e26102c79ffa0e773ed14fc75e53e5bd9
lacie-life <00sao00ios00@gmail.com>
2020-06-05 14:53:28 +0700

    add simple read wather station

--- weatherStattion/weatherStattion.ino
+++ weatherStattion/weatherStattion.ino
@@ -1,17 +1,13 @@
+#include <SPI.h>
+#include <RH_RF95.h>
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(10, 11); // RX, TX
 
-void setup() {
-  // Pin for relay module set as output
-//  pinMode(relay, OUTPUT);
-  Serial.begin(9600);
-  mySerial.begin(9600);
-//  digitalWrite(relay, LOW);
-}
-
+// Singleton instance of the radio driver
+RH_RF95 rf95;
+float frequency = 868.0;
 char                 databuffer[35];
-double               temp;
 
 void getBuffer()                                                                    //Get weather status data
 {
@@ -31,95 +27,65 @@ void getBuffer()                                                                
       index --;
     }
   }
+  Serial.println(databuffer);
 }
 
-int transCharToInt(char *_buffer,int _start,int _stop)                               //char to int）
+void setup() 
 {
-  int _index;
-  int result = 0;
-  int num = _stop - _start + 1;
-  int _temp[num];
-  for (_index = _start;_index <= _stop;_index ++)
-  {
-    _temp[_index - _start] = _buffer[_index] - '0';
-    result = 10*result + _temp[_index - _start];
-  }
-  return result;
-}
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  //while (!Serial) ; // Wait for serial port to be available
+  Serial.println("Start LoRa Client");
+  if (!rf95.init())
+    Serial.println("init failed");
+  // Setup ISM frequency
+  rf95.setFrequency(frequency);
+  // Setup Power,dBm
+  rf95.setTxPower(13);
 
-int WindDirection()                                                                  //Wind Direction
-{
-  return transCharToInt(databuffer,1,3);
-}
-
-float WindSpeedAverage()                                                             //air Speed (1 minute)
-{
-  temp = 0.44704 * transCharToInt(databuffer,5,7);
-  return temp;
-}
-
-float WindSpeedMax()                                                                 //Max air speed (5 minutes)
-{
-  temp = 0.44704 * transCharToInt(databuffer,9,11);
-  return temp;
-}
-
-float Temperature()                                                                  //Temperature ("C")
-{
-  temp = (transCharToInt(databuffer,13,15) - 32.00) * 5.00 / 9.00;
-  return temp;
-}
-
-float RainfallOneHour()                                                              //Rainfall (1 hour)
-{
-  temp = transCharToInt(databuffer,17,19) * 25.40 * 0.01;
-  return temp;
-}
-
-float RainfallOneDay()                                                               //Rainfall (24 hours)
-{
-  temp = transCharToInt(databuffer,21,23) * 25.40 * 0.01;
-  return temp;
-}
-
-int Humidity()                                                                       //Humidity
-{
-  return transCharToInt(databuffer,25,26);
-}
-
-float BarPressure()                                                                  //Barometric Pressure
-{
-  temp = transCharToInt(databuffer,28,32);
-  return temp / 10.00;
+  // Setup Spreading Factor (6 ~ 12)
+  rf95.setSpreadingFactor(7);
+  
+  // Setup BandWidth, option: 7800,10400,15600,20800,31200,41700,62500,125000,250000,500000
+  //Lower BandWidth for longer distance.
+  rf95.setSignalBandwidth(125000);
+  
+  // Setup Coding Rate:5(4/5),6(4/6),7(4/7),8(4/8) 
+  rf95.setCodingRate4(5);
 }
 
 void loop()
 {
-  getBuffer();                                                                      //Begin!
-  Serial.print("Wind Direction: ");
-  Serial.print(WindDirection());
-  Serial.println("  ");
-  Serial.print("Average Wind Speed (One Minute): ");
-  Serial.print(WindSpeedAverage());
-  Serial.println("m/s  ");
-  Serial.print("Max Wind Speed (Five Minutes): ");
-  Serial.print(WindSpeedMax());
-  Serial.println("m/s");
-  Serial.print("Rain Fall (One Hour): ");
-  Serial.print(RainfallOneHour());
-  Serial.println("mm  ");
-  Serial.print("Rain Fall (24 Hour): ");
-  Serial.print(RainfallOneDay());
-  Serial.println("mm");
-  Serial.print("Temperature: ");
-  Serial.print(Temperature());
-  Serial.println("C  ");
-  Serial.print("Humidity: ");
-  Serial.print(Humidity());
-  Serial.println("%  ");
-  Serial.print("Barometric Pressure: ");
-  Serial.print(BarPressure());
-  Serial.println("hPa");
-  Serial.println("");
-   Serial.println("");
+  Serial.println("Sending to LoRa Server");
+  // Send a message to LoRa Server
+  getBuffer();
+  uint8_t data[] = "Hello, this is device 1";
+  rf95.send(databuffer, sizeof(databuffer));
+  
+  rf95.waitPacketSent();
+//  Serial.println(databuffer);
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  if (rf95.waitAvailableTimeout(3000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is LoRa server running?");
+  }
+  delay(5000);
 }
